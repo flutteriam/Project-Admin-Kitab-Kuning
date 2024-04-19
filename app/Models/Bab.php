dde91f81d971b0d0d2b134a079c9e3eeb533c355
iam <imam.am28@gmail.com>
2024-04-19 10:34:19 +0700

    soft deleted

--- app/Models/Bab.php
+++ app/Models/Bab.php
@@ -4,9 +4,11 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Bab extends Model
 {
+    use SoftDeletes;
     protected $table = "babs";
     protected $fillable = ['book_id', 'order', 'title', 'translate_title'];
 
--- app/Models/Chapter.php
+++ app/Models/Chapter.php
@@ -4,9 +4,11 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Chapter extends Model
 {
+    use SoftDeletes;
     protected $table = "chapters";
     protected $fillable = [
         'book_id',
