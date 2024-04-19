c812963efc43e2cb73543d84425b020d90a17c4a
iam <imam.am28@gmail.com>
2024-04-19 09:32:41 +0700

    soft delete book

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -5,9 +5,12 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Book extends Model
 {
+    use SoftDeletes;
+
     protected $table = 'books';
 
     public $timestamps = true;
